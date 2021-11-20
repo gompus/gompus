4ffc5981a65e7836e78581c865b01682b0b20f35
Daspoet <cederk2306@gmail.com>
2021-11-20 01:47:37 +0100

    feat(app flags): use better string method

--- models/application/flags.go
+++ models/application/flags.go
@@ -1,6 +1,6 @@
 package application
 
-//go:generate stringer -type=Flag
+import "strings"
 
 // Flags describe an Application.
 type Flags int
@@ -15,3 +15,25 @@ const (
 	FlagsGatewayMessageContent               = 1 << 18
 	FlagsGatewayMessageContentLimited        = 1 << 19
 )
+
+var flagToStr = map[Flags]string{
+	FlagsGatewayPresence:               "GatewayPresence",
+	FlagsGatewayPresenceLimited:        "GatewayPresenceLimited",
+	FlagsGatewayGuildMembers:           "GatewayGuildMembers",
+	FlagsGatewayGuildMembersLimited:    "GatewayGuildMembersLimited",
+	FlagsVerificationPendingGuildLimit: "VerificationPendingGuildLimit",
+	FlagsEmbedded:                      "Embedded",
+	FlagsGatewayMessageContent:         "GatewayMessageContent",
+	FlagsGatewayMessageContentLimited:  "GatewayMessageContentLimited",
+}
+
+// String returns a string representation of f.
+func (f Flags) String() string {
+	var out []string
+	for flag, s := range flagToStr {
+		if f&flag != 0 {
+			out = append(out, s)
+		}
+	}
+	return strings.Join(out, "|")
+}
