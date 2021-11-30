771a54dc45f856815edab920a472a0c2e6bf140f
Daspoet <cederk2306@gmail.com>
2021-11-30 23:16:28 +0100

    feat(rest): add invites

---
+++ rest/invites/delete.go
@@ -0,0 +1,19 @@
+package invites
+
+import (
+	"fmt"
+	"github.com/gompus/gompus/models/invite"
+	"github.com/gompus/gompus/rest/client"
+	"github.com/gompus/gompus/rest/client/auth"
+)
+
+// Delete deletes the invite with the given code.
+func Delete(token auth.Token, code string) (invite invite.Invite, err error) {
+	return invite, client.DELETE(client.Request{
+		Path: fmt.Sprintf("/invites/%s", code),
+		Headers: []client.HeaderFunc{
+			client.AuthHeader(token),
+		},
+		Result: &invite,
+	})
+}
---
+++ rest/invites/get.go
@@ -0,0 +1,19 @@
+package invites
+
+import (
+	"fmt"
+	"github.com/gompus/gompus/models/invite"
+	"github.com/gompus/gompus/rest/client"
+	"github.com/gompus/gompus/rest/client/auth"
+)
+
+// Get retrieves an invite for the given code.
+func Get(token auth.Token, code string) (invite invite.Invite, err error) {
+	return invite, client.GET(client.Request{
+		Path: fmt.Sprintf("/invites/%s", code),
+		Headers: []client.HeaderFunc{
+			client.AuthHeader(token),
+		},
+		Result: &invite,
+	})
+}
