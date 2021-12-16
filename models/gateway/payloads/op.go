02b306fa68e7655a92753f7813aa84d7c721cb8b
Daspoet <cederk2306@gmail.com>
2021-12-16 01:37:52 +0100

    feat(gateway): add opcodes

--- models/gateway/payloads/op.go
+++ models/gateway/payloads/op.go
@@ -1,4 +1,20 @@
 package payloads
 
+//go:generate stringer -type=Op -trimprefix=Op
+
 // Op represents a payload's opcode.
 type Op int
+
+const (
+	OpDispatch            Op = 0
+	OpHeartbeat              = 1
+	OpIdentify               = 2
+	OpPresenceUpdate         = 3
+	OpVoiceStateUpdate       = 4
+	OpResume                 = 6
+	OpReconnect              = 7
+	OpRequestGuildMembers    = 8
+	OpInvalidSession         = 9
+	OpHelloReceive           = 10
+	OpHeartbeatACK           = 11
+)
